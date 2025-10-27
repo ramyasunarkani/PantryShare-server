f43366b608762e01c6d566b688257d4fd178d86c
ramyasunarkani <ramyakrishnasunarkani@gmail.com>
2025-10-27 12:06:29 +0530

    server

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/reservations',reservationRoute);
 (async () => {
   try {
     await connectDB();
-    app.listen(3000, () => {
+    app.listen(process.env.PORT, () => {
       console.log('Server running on port 3000');
     });
   } catch (error) {
